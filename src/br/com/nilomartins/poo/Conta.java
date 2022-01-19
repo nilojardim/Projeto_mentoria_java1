e0fe77f09705e70aa921aeae79470c74e9807795
nilojardim <97597953+nilojardim@users.noreply.github.com>
2022-01-19 02:48:50 -0300

    Aula de métodos com parametros

---
+++ src/br/com/nilomartins/poo/Conta.java
@@ -0,0 +1,24 @@
+package br.com.nilomartins.poo;
+
+public class Conta {
+
+    String cliente;
+    double saldo;
+
+    void exibeSaldo(){
+        System.out.println(cliente + " seu saldo é " + saldo);
+    }
+
+    void saca (double valor){
+        saldo -= valor;
+    }
+
+    void deposita (double valor){
+        saldo += valor;
+    }
+
+    void transferePara (Conta destino, double valor){
+        this.saca(valor);
+        destino.deposita(valor);
+    }
+}
---
+++ src/br/com/nilomartins/poo/ContaTest.java
@@ -0,0 +1,22 @@
+package br.com.nilomartins.poo;
+
+public class ContaTest {
+
+    public static void main(String[] args) {
+
+        Conta conta = new Conta();
+        conta.cliente = "Nilo";
+        conta.saldo = 10_000.00;
+        conta.exibeSaldo();
+
+        Conta destino = new Conta();
+        destino.cliente = "Lawrence";
+        destino.saldo = 8_000.00;
+        destino.exibeSaldo();
+
+        conta.transferePara(destino,1550.00);
+        conta.exibeSaldo();
+        destino.exibeSaldo();
+
+    }
+}
