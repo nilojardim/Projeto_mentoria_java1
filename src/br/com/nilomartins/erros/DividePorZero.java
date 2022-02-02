feccf9c57bec65a903fff14c8b11458a144a64bc
nilojardim <97597953+nilojardim@users.noreply.github.com>
2022-02-01 21:09:53 -0300

    Aula de métodos sobrecarregados

--- src/br/com/nilomartins/erros/DividePorZero.java
+++ src/br/com/nilomartins/erros/DividePorZero.java
@@ -4,6 +4,19 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DividePorZero {
+
+    public static void dividir(Scanner s) throws InputMismatchException, ArithmeticException
+    {
+
+        System.out.print("Numero: ");
+        int a = s.nextInt();
+        System.out.print("Divisor: ");
+        int b = s.nextInt();
+
+        System.out.println(a / b);
+
+    }
+
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
@@ -11,20 +24,20 @@ public class DividePorZero {
 
         do {
             try {
-                System.out.print("Numero: ");
-                int a = s.nextInt();
-                System.out.print("Divisor: ");
-                int b = s.nextInt();
 
+                dividir(s);
 
-                System.out.println(a / b);
                 continua = false;
-            } catch (InputMismatchException e1) {
-                System.err.println("Numeros devem ser inteiros");
+
+            } catch
+
+                (InputMismatchException | ArithmeticException e1) {
+                System.err.println("Numero Invalido");
+                e1.getMessage();
                 s.nextLine();
-            } catch (ArithmeticException e2) {
-                System.err.println("Divisor deve ser diferente de zero");
+
             }
+
             finally {
                 System.out.println("Finally Executado...");
             }
---
+++ src/br/com/nilomartins/erros/ExcecoesComuns.java
@@ -0,0 +1,27 @@
+package br.com.nilomartins.erros;
+
+public class ExcecoesComuns {
+
+    static int [] arrayNull = new int[0];
+
+    public static void main(String[] args) {
+
+        //NullPointerException
+        //System.out.println(arrayNull.length);
+
+        //ArithmeticException
+        //int x = 5 / 0;
+
+        //ArrayIndexOutOfBoundsException
+        //System.out.println(arrayNull[1]);
+
+        //ClassCastException
+        //Animal a = new Galinha();
+        //Cachorro c  = (Cachorro) a;
+
+        //NumberFormatException
+        //double d = Double.parseDouble("XIT");
+
+
+    }
+}
